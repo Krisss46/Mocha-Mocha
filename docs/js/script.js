1660658a00754368fd0e30465bc92326b996ae2b
Krisss46 <kristjan.kristjan@hotmail.com>
2025-01-27 15:54:00 +0000

    added order notifications to menus

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,21 @@
-function order(price){
- console.log("aa");
+// enables order added popup
+function order(price, product) {
+ console.log("Order function :: price: ", price, ", product: ", product);
+
+ const div = document.createElement("div");
+ div.className = "notification";
+
+ const newMsg = document.createElement("p");
+ newMsg.innerHTML = `${product} / ${price}$ added to cart`;
+
+ div.appendChild(newMsg);
+ document.body.appendChild(div);
+
+ setTimeout(() => {
+  div.style.opacity = "0";
+ }, 1200);
+
+ div.addEventListener("transitionend", () => {
+   div.remove();
+ });
 }
